aebf4a8ed8d8c35f54b79891a685375471076241
Joe Plonsker <joeplonsker@Joes-MacBook-Pro.local>
2017-03-24 16:29:34 -0400

    add purple again to the middle of colors array

--- color-change.js
+++ color-change.js
@@ -1,4 +1,4 @@
-var colors = ['#c6acc9', '#ecb4bf', '#fdd6b5','#fdf3b8','#a7e4ae','#bfffe6', '#8699d1', '#dba9ce', '#aae7e8', '#efe6eb', '#47d1d5', '#e8d2ff', '#c1d1fd', '#d9e0fc', '#ffe0f1','purple'];
+var colors = ['#c6acc9', '#ecb4bf', '#fdd6b5','#fdf3b8','#a7e4ae','#bfffe6', '#8699d1', '#dba9ce', '#aae7e8', '#efe6eb', 'purple', '#47d1d5', '#e8d2ff', '#c1d1fd', '#d9e0fc', '#ffe0f1'];
 var active = 0;
 setInterval(function(){
     document.querySelector('body').style.background = colors[active];
